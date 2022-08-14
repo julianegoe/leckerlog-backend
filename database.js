fba5f819d6cc0caee416445804e81067409988c0
julianegoe <goersch.juliane@dpa.com>
2022-08-14 15:45:54 +0200

    test

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.USER,
+    database_url: process.env.DATABASE_URL,
+    /* user: process.env.USER,
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DB_POST,
     databse: process.env.DATABASE,
-    dialect: "postgres",
+    dialect: "postgres", */
 }); 
 
 pool.on('connect', () => console.log('connected to db'));
