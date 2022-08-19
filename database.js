a5d1412ccfa4bade6319a1409e2e8082432433de
julianegoe <goersch.juliane@dpa.com>
2022-08-19 18:12:35 +0200

    auth firebase

--- database.js
+++ database.js
@@ -3,12 +3,6 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    /* user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DB_POST,
-    databse: process.env.DATABASE,
-    dialect: "postgres", */
 }); 
 
 pool.on('connect', () => console.log('connected to db'));
--- index.js
+++ index.js
@@ -3,14 +3,44 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./database');
+var logger = require('morgan');
+const admin = require('firebase-admin');
+var serviceAccount = require("./config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://leckerlog-default-rtdb.europe-west1.firebasedatabase.app"
+});
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(logger('dev'));
+
+const checkAuth = (req, res, next) => {
+    if (req.headers.authtoken) {
+        admin.auth().verifyIdToken(req.headers.authtoken)
+            .then(() => {
+                next();
+            }).catch((error) => {
+                console.log(error);
+                res.status(403).send('Unauthorized')
+            });
+    } else {
+        res.status(403).send('Unauthorized')
+    }
+};
+
+app.use('/', checkAuth);
+app.use('/cuisines', checkAuth);
+app.use('/restaurants', checkAuth);
+app.use('/food', checkAuth);
 
 // routes
 app.get('/', (_, res) => {
-    res.send('Hello World')
+    res.json({
+        message: 'hello world',
+    })
 })
 
 // get all cuisines
@@ -19,11 +49,11 @@ app.get('/cuisines', async (req, res) => {
         const client = await pool.connect();
         const restaurants = await client.query('SELECT * from cuisines');
         res.json(restaurants.rows);
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
@@ -31,17 +61,17 @@ app.get('/cuisines', async (req, res) => {
 app.post('/restaurants', async (req, res) => {
     try {
         const client = await pool.connect();
-        const { name, cuisine, cuisine_id} = req.body;
+        const { name, cuisine, cuisine_id } = req.body;
         const date_created = new Date().toISOString().split('T')[0];
         const date_updated = new Date().toISOString().split('T')[0];
         const restaurants = await client.query("INSERT INTO restaurants(name, cuisine, cuisine_Id, date_created, date_updated) VALUES($1, $2, $3, $4, $5) RETURNING *",
-        [name, cuisine, cuisine_id, date_created, date_updated]);
+            [name, cuisine, cuisine_id, date_created, date_updated]);
         res.json(restaurants.rows);
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
@@ -49,17 +79,17 @@ app.post('/restaurants', async (req, res) => {
 app.post('/food', async (req, res) => {
     try {
         const client = await pool.connect();
-        const { name, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path} = req.body;
+        const { name, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path } = req.body;
         const date_created = new Date().toISOString().split('T')[0];
         const date_updated = new Date().toISOString().split('T')[0];
         const restaurants = await client.query("INSERT INTO food_ordered(name, cuisine_Id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
-        [name, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated]);
+            [name, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated]);
         res.json(restaurants.rows);
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
@@ -69,11 +99,11 @@ app.get('/restaurants', async (req, res) => {
         const client = await pool.connect();
         const restaurants = await client.query('SELECT * from restaurants');
         res.json(restaurants.rows);
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
@@ -83,11 +113,11 @@ app.get('/food', async (req, res) => {
         const client = await pool.connect();
         const foodOrdered = await client.query('SELECT * from food_ordered');
         res.json(foodOrdered.rows);
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
@@ -98,11 +128,11 @@ app.get('/restaurants/:id', async (req, res) => {
         const { id } = req.params;
         const restaurants = await client.query('SELECT * from restaurants WHERE restaurant_id = $1', [id]);
         res.json(restaurants.rows);
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
@@ -113,11 +143,11 @@ app.delete('/food/:id', async (req, res) => {
         const { id } = req.params;
         const restaurants = await client.query('DELETE from food_ordered WHERE food_id = $1', [id]);
         res.send('Food was successfully deleted');
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         res.status(500).send({
             message: error.message || "Some error occurred.",
-          });
+        });
     }
 })
 
