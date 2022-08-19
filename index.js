b51e607de67e7e498974435d8c1410264cd253ed
julianegoe <goersch.juliane@dpa.com>
2022-08-19 22:49:21 +0200

    post req

--- index.js
+++ index.js
@@ -59,11 +59,12 @@ app.post('/restaurants/:id', async (req, res) => {
         const { name, cuisine } = req.body;
         const { id } = req.params;
         const cuisine_id = await pool.query('SELECT cuisine_id from cuisines where name = $1', [cuisine]);
+        console.log(cuisine_id.rows);
         const date_created = new Date().toISOString().split('T')[0];
         const date_updated = new Date().toISOString().split('T')[0];
         const existingRestaurant = await pool.query('SELECT * from restaurants where name = $1 and user_id = $2', [name, id])
         if (!existingRestaurant) {
-            const addedRestaurant = await pool.query("INSERT INTO restaurants(name, cuisine, cuisine_Id, date_created, date_updated, user_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *", [name, cuisine, cuisine_id.rows[0].cuisine_id, date_created, date_updated, id]);
+            const addedRestaurant = await pool.query("INSERT INTO restaurants(name, cuisine, cuisine_Id, date_created, date_updated, user_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *", [name, cuisine, 12, date_created, date_updated, id]);
             res.json(addedRestaurant.rows);
         }
     } catch (error) {
@@ -80,12 +81,12 @@ app.post('/food/:id', async (req, res) => {
         const { name, cuisine, restaurant_id, comment, rating, ordered_at, image_path } = req.body;
         const { id } = req.params;
         const cuisine_id = await pool.query('SELECT cuisine_id from cuisines where name = $1', [cuisine]);
-        console.log(cuisine_id.rows[0].cuisine_id);
+        console.log(cuisine_id.rows);
         const date_created = new Date().toISOString().split('T')[0];
         const date_updated = new Date().toISOString().split('T')[0];
-        const restaurants = await pool.query("INSERT INTO food_ordered(name, user_id, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
-            [name, id, cuisine_id.rows[0].cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated]);
-        res.json(restaurants.rows);
+        const foodOrdered = await pool.query("INSERT INTO food_ordered(name, user_id, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+            [name, id, 12, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated]);
+        res.json(foodOrdered.rows);
     } catch (error) {
         console.log(error)
         res.status(500).send({
