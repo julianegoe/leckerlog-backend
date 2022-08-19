696ca447f874f2b7c66a583cdadc7214413681e4
julianegoe <goersch.juliane@dpa.com>
2022-08-19 18:49:55 +0200

    get user

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(logger('dev'));
 
 const checkAuth = (req, res, next) => {
     if (req.headers.authtoken) {
-        admin.auth().verifyIdToken(req.headers.authtoken)
+        admin.auth().verifyIdToken(req.headers.authtoken);
+        console.log(admin.auth().getUser(req.body.id))
             .then(() => {
                 next();
             }).catch((error) => {
