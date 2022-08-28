5d441a1ef55dd2af7e9ab77c7f9b4a32255ac0d4
julianegoe <goersch.juliane@dpa.com>
2022-08-28 23:16:56 +0200

    cors

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ var logger = require('morgan');
 const admin = require('firebase-admin');
 const morgan = require('morgan');
 
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://own-backend--fantastic-fairy-abcd7e.netlify.app'],
+  }
+
 admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(process.env.FB_SERVICE_ACCOUNT_KEY)),
     databaseURL: process.env.FB_DATABASE_URL,
 });
 
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan('common'));
 app.use(express.json());
 app.use(logger('dev'));
