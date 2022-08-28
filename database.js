090f0d7e0e06e0610b902cd9d75f3e9a49182750
julianegoe <goersch.juliane@dpa.com>
2022-08-28 19:25:38 +0200

    made functions in db

--- database.js
+++ database.js
@@ -3,9 +3,68 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-}); 
+});
+
+const getCuisinesForUser = async (userId) => {
+    return await pool.query(`
+    SELECT * from cuisines where cuisine_Id in (SELECT cuisine_id
+    FROM restaurants
+    WHERE user_id = $1);
+    `, [userId]);
+}
+
+const getAllCuisines = async () => {
+    return await pool.query(`
+    SELECT * from cuisines
+    `);
+}
+
+const addNewCuisine = async (cuisineName) => {
+    return await pool.query("INSERT INTO cuisines(name) VALUES($1) RETURNING *", [cuisineName]);
+}
+
+const getLeckerlog = async (userId) => {
+    return await pool.query(`
+    SELECT restaurants.*, subVirt.food_ordered
+    FROM restaurants
+    LEFT JOIN (SELECT restaurant_id, json_agg(row_to_json(food_ordered)) AS food_ordered FROM  food_ordered WHERE user_id = $1
+    GROUP  BY 1
+    ) subVirt ON subVirt.restaurant_id = restaurants.restaurant_id;`, [userId]);
+}
+
+const addOrUpdateRestaurant = async (restaurantName, cuisine, cuisine_id, date_created, date_updated, userId) => {
+    await db.pool.query(`
+        INSERT INTO restaurants (name, cuisine, cuisine_Id, date_created, date_updated, user_id)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        ON CONFLICT (name, user_id) DO UPDATE
+        SET 
+        name = $1,
+        cuisine = $2,
+        cuisine_Id = $3,
+        date_updated =  $5 RETURNING *;`, 
+        [
+            restaurantName, cuisine, cuisine_id, date_created, date_updated, userId,
+        ]);
+};
+
+const addFoodOrdered = async (foodName, userId, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated) => {
+    return await db.pool.query("INSERT INTO food_ordered (name, user_id, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+    [foodName, userId, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated]);
+}
+
+const deleteFoodOrdered = async (userId, foodId) => {
+    return await pool.query('DELETE from food_ordered WHERE user_id = $1 and food_id = $2 RETURNING *', [userId, foodId]);
+}
 
 pool.on('connect', () => console.log('connected to db'));
 
-
-module.exports = pool;
+module.exports = {
+    pool,
+    getLeckerlog,
+    getAllCuisines,
+    addNewCuisine,
+    getCuisinesForUser,
+    addOrUpdateRestaurant,
+    addFoodOrdered,
+    deleteFoodOrdered,
+};
--- index.js
+++ index.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const pool = require('./database');
+const db = require('./database');
 var logger = require('morgan');
 const admin = require('firebase-admin');
+const morgan = require('morgan');
 
 admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(process.env.FB_SERVICE_ACCOUNT_KEY)),
-    databaseURL: "https://leckerlog-default-rtdb.europe-west1.firebasedatabase.app"
+    databaseURL: process.env.FB_DATABASE_URL,
 });
 
 // middleware
 app.use(cors());
+app.use(morgan('common'));
 app.use(express.json());
 app.use(logger('dev'));
 
@@ -46,11 +48,7 @@ app.get('/', (_, res) => {
 app.get('/cuisines/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const cuisines = await pool.query(`
-        SELECT * from cuisines where cuisine_Id in (SELECT cuisine_id
-        FROM restaurants
-        WHERE user_id = $1);
-        `, [id]);
+        const cuisines = await db.getCuisinesForUser(id);
         res.json(cuisines.rows);
     } catch (error) {
         console.log(error)
@@ -63,9 +61,7 @@ app.get('/cuisines/:id', async (req, res) => {
 // get all cuisines
 app.get('/cuisines', async (req, res) => {
     try {
-        const cuisines = await pool.query(`
-        SELECT * from cuisines
-        `);
+        const cuisines = await db.getAllCuisines()
         res.json(cuisines.rows);
     } catch (error) {
         console.log(error)
@@ -76,10 +72,10 @@ app.get('/cuisines', async (req, res) => {
 })
 
 // add a cuisine
-app.post('/cuisines/:name', async (req, res) => {
+app.put('/cuisines/:name', async (req, res) => {
     const { name } = req.params;
     try {
-        const addedCuisine = await pool.query("INSERT INTO cuisines(name) VALUES($1) RETURNING *", [name]);
+        const addedCuisine = await db.addNewCuisine(name);
         res.json(addedCuisine.rows);
     } catch (error) {
         console.log(error)
@@ -89,27 +85,16 @@ app.post('/cuisines/:name', async (req, res) => {
     }
 })
 
-// create a restaurant record
-app.post('/restaurants/:id', async (req, res) => {
+// create a leckerlog record
+app.post('/leckerlog/:id', async (req, res) => {
     try {
         const { restaurantName, foodName, cuisine, cuisine_id, comment, rating, ordered_at, image_path } = req.body;
         const { id } = req.params;
         const date_created = new Date().toISOString().split('T')[0];
         const date_updated = new Date().toISOString().split('T')[0];
-        const updatedRestaurant = await pool.query(`
-        INSERT INTO restaurants (name, cuisine, cuisine_Id, date_created, date_updated, user_id)
-        VALUES ($1, $2, $3, $4, $5, $6)
-        ON CONFLICT (name, user_id) DO UPDATE
-        SET cuisine = $2,
-        cuisine_Id = $3,
-        date_updated =  $5 RETURNING *;`, 
-        [
-            restaurantName, cuisine, cuisine_id, date_created, date_updated, id,
-        ])
-        console.log(updatedRestaurant.rows)
+        const updatedRestaurant = await db.addOrUpdateRestaurant(restaurantName, cuisine, cuisine_id, date_created, date_updated, id)
         if (updatedRestaurant.rows[0].restaurant_id) {
-            const foodOrdered = await pool.query("INSERT INTO food_ordered(name, user_id, cuisine_id, restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
-            [foodName, id, cuisine_id, updatedRestaurant.rows[0].restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated]);
+            const foodOrdered = await db.addFoodOrdered(foodName, userId, cuisine_id, updatedRestaurant.rows[0].restaurant_id, comment, rating, ordered_at, image_path, date_created, date_updated)
             res.json([updatedRestaurant.rows, foodOrdered.rows]);
         }
     } catch (error) {
@@ -120,16 +105,11 @@ app.post('/restaurants/:id', async (req, res) => {
     }
 })
 
-// get all restaurants and food for user
+// get all leckerlogs for user
 app.get('/leckerlog/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const restaurants = await pool.query(`
-        SELECT restaurants.*, subVirt.food_ordered
-        FROM restaurants
-        LEFT JOIN (SELECT restaurant_id, json_agg(row_to_json(food_ordered)) AS food_ordered FROM  food_ordered WHERE user_id = $1
-        GROUP  BY 1
-        ) subVirt ON subVirt.restaurant_id = restaurants.restaurant_id;`, [id]);
+        const restaurants = await db.getLeckerlog(id);
         res.json(restaurants.rows);
     } catch (error) {
         console.log(error)
@@ -139,25 +119,11 @@ app.get('/leckerlog/:id', async (req, res) => {
     }
 })
 
-// get all food for user
-app.get('/food/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const foodOrdered = await pool.query('SELECT * from food_ordered where user_id = $1', [id]);
-        res.json(foodOrdered.rows);
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            message: error.message || "Some error occurred.",
-        });
-    }
-})
-
-// delete food for restaurant for user
+// delete food for user
 app.delete('/food/:id/:foodId', async (req, res) => {
     try {
         const { id, foodId } = req.params;
-        const restaurants = await pool.query('DELETE from food_ordered WHERE user_id = $1 and food_id = $2 RETURNING *', [id, foodId]);
+        const restaurants = await db.deleteFoodOrdered(id, foodId);
         res.send(restaurants.rows);
     } catch (error) {
         console.log(error)
@@ -166,7 +132,5 @@ app.delete('/food/:id/:foodId', async (req, res) => {
         });
     }
 })
-
-// delete restaurant for user
 
 app.listen(process.env.PORT || 8080, () => console.log('listening to Port' + process.env.PORT));
