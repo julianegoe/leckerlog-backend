3574fb161ada308a976ad7551a7abfc4fbf51847
Juliane Görsch <goersch.juliane@dpa.com>
2022-10-15 21:15:09 +0200

    correctly update cuisine

--- database.js
+++ database.js
@@ -67,6 +67,11 @@ const updateFoodOrdered = async (name, cuisines_id, rating, comment, tags, food_
         [name, cuisines_id, rating, comment, tags, food_id, user_id, date_updated]);
 }
 
+const updateRestaurantCuisine = async (cuisine_id, restaurant_id, user_id) => {
+    return await pool.query('UPDATE restaurants SET cuisine_id = $1, cuisine = (SELECT name from cuisines WHERE cuisines.cuisine_id = $1) WHERE restaurant_id = $2 and user_id = $3 RETURNING *',
+        [cuisine_id, restaurant_id, user_id ]);
+}
+
 const updateImagePath = async (image_path, ordered_at, address, restaurant_id, food_id, user_id) => {
     return await pool.query('UPDATE food_ordered SET image_path = $1, ordered_at = $2  WHERE food_id = $5 and user_id = $6; UPDATE restaurants SET address = $3 WHERE restaurant_id = $4 and user_id = $6',
         [image_path, ordered_at, address, restaurant_id, food_id, user_id]);
@@ -86,4 +91,5 @@ module.exports = {
     getFoodOrdered,
     updateFoodOrdered,
     updateImagePath,
+    updateRestaurantCuisine,
 };
--- index.js
+++ index.js
@@ -149,12 +149,13 @@ app.get('/food/:id/:foodId', async (req, res) => {
 });
 
 //update food ordered
-app.post('/food/:foodId/:userId', async (req, res) => {
+app.post('/food/:foodId/:restaurantId/:userId', async (req, res) => {
     try {
         const { name, rating, comment, cuisine_id, tags } = req.body;
-        const { userId, foodId } = req.params;
+        const { userId, foodId, restaurantId } = req.params;
         const date_updated = new Date().toISOString().split('T')[0];
-        const foodOrdered = await db.updateFoodOrdered(name, cuisine_id, rating, comment, tags, foodId, userId, date_updated);
+        const foodOrdered = await db.updateFoodOrdered(name, cuisine_id, rating, comment, tags, foodId, userId, date_updated, restaurantId);
+        await db.updateRestaurantCuisine(cuisine_id, restaurantId, userId);
         res.send(foodOrdered.rows);
     } catch (error) {
         console.log(error)
